a9ca14705b0106be139772e179b77e59c8ed2832
madinaerrr <mabdullaeva011@gmail.com>
2022-02-28 16:48:38 +0400

    chairs
    chairs

---
+++ chairs/grids.js
@@ -0,0 +1,16 @@
+/* const button = document.querySelector('#btn3');
+const cards = document.querySelector('#cards4')
+
+
+button.addEventListener("click", function(){
+
+cards.classList.toggle('hidden');
+
+if (cards.classList.contains('hidden')) {
+    button.textContent = "Подробнее"
+} else { button.textContent = "Скрыть" }
+
+})
+
+ */
+
